fec1a039747ea45de063948ed90b25d24a5978d5
Shailendra Ydv <106907272+shailendra40@users.noreply.github.com>
2024-07-12 23:12:04 +0545

    video recorded fetch to play

--- resources/views/videos/index.blade.php
+++ resources/views/videos/index.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html>
 <head>
     <title>Videos</title>
@@ -18,5 +18,36 @@
     </ul>
 
     <a href="{{ route('videos.create') }}">Upload New Video</a>
+</body>
+</html> -->
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Videos</title>
+</head>
+<body>
+    <h1>Video List</h1>
+    @if(session('success'))
+        <p>{{ session('success') }}</p>
+    @endif
+
+    <ul>
+        @forelse($videos as $video)
+            <li>
+                <h3>{{ $video->title }}</h3>
+                <p>{{ $video->description }}</p>
+                <video width="320" height="240" controls>
+                    <source src="{{ asset('storage/' . $video->video_path) }}" type="video/{{ pathinfo($video->video_path, PATHINFO_EXTENSION) }}">
+                    Your browser does not support the video tag.
+                </video>
+            </li>
+        @empty
+            <li>No videos available</li>
+        @endforelse
+    </ul>
+
+    <a href="{{ route('videos.create') }}">Upload or Record New Video</a>
 </body>
 </html>
